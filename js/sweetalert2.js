4e083c471b2aca3236f53dd00c9521f4253e1bdd
youssefghannam1 <mryoussefghannam@gmail.com>
2025-02-12 14:15:54 +0100

    initial Commit

---
+++ js/sweetalert2.js
@@ -0,0 +1,4 @@
+import Swal from 'sweetalert2'
+
+// or via CommonJS
+const Swal = require('sweetalert2')
---
+++ sweetalert2.js
@@ -0,0 +1,7 @@
+import Swal from './node_modules/sweetalert2/dist/sweetalert2'
+import './node_modulessweetalert2/src/sweetalert2.scss'
+
+// or via CommonJS
+const Swal = require('sweetalert2')
+
+export default Swal
